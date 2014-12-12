93b15ca26a27852a1ff8bc3fc4f5763813559c38
danishkhan <dk@danishkhan.org>
2014-12-12 11:59:03 -0800

    made test simple

--- test.rb
+++ test.rb
@@ -14,11 +14,6 @@ class HelloWorldTest < Test::Unit::TestCase
   def test_it_says_hello_world
     get '/'
     assert last_response.ok?
-    assert_equal 'Hello World', last_response.body
-  end
-
-  def test_it_says_hello_to_a_person
-    get '/', :name => 'Hubot'
-    assert last_response.body.include?('Hubot')
+    assert_equal 'Hello Hubot', last_response.body
   end
 end
