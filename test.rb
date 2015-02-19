27742301daab5bdc2abecbef2f4458ae0629aa00
Danish Khan <dk@danishkhan.org>
2015-02-19 12:31:50 -0800

    updated the test file

--- test.rb
+++ test.rb
@@ -14,6 +14,6 @@ class HelloWorldTest < Test::Unit::TestCase
   def test_it_says_hello_world
     get '/'
     assert last_response.ok?
-    assert_equal 'Hello McGraw Hill', last_response.body
+    assert_equal 'Hello Danish', last_response.body
   end
 end
