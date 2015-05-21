78ac8b1ab595c599eb786934aabbec39b2c12f25
Steve Jobs <octodemo+technical-lead@github.com>
2015-05-21 09:54:52 -0700

    Update test.rb

--- test.rb
+++ test.rb
@@ -14,6 +14,6 @@ class HelloWorldTest < Test::Unit::TestCase
   def test_it_says_hello_world
     get '/'
     assert last_response.ok?
-    assert last_response.body.include?('Hello, Danish')
+    assert last_response.body.include?('Hello, Bob')
   end
 end
